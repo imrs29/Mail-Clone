ebe352f73950a55ee27946f42a569d57108cd049
imrs29 <74778063+imrs29@users.noreply.github.com>
2021-11-08 16:01:30 +0530

    added

---
+++ src/firebase.js
@@ -0,0 +1,22 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+
+import firebase from "firebase";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyB7vWkHz0Cmt3iUtApZW1Av91Wv-jVUMuU",
+  authDomain: "clone-ritu.firebaseapp.com",
+  projectId: "clone-ritu",
+  storageBucket: "clone-ritu.appspot.com",
+  messagingSenderId: "308005654921",
+  appId: "1:308005654921:web:cbfb5216faa14463e35158",
+  measurementId: "G-G7GQ3YLC7D",
+};
+
+const firebaseApp = firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const provider = new firebase.auth.GoogleAuthProvider();
+
+const db = firebaseApp.firestore();
+
+export { auth, provider };
+export default db;
